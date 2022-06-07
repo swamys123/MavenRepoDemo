6b25525510c6354db07f70602cdf5da62d3ad97c
N S Swamys <nsswamys@gmail.com>
2022-06-07 17:25:23 +0530

    Pushed MavenDemo Project

---
+++ src/main/java/com/projectRepo/App.java
@@ -0,0 +1,13 @@
+package com.projectRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello to Project Repo !" );
+    }
+}
